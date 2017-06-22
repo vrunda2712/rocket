16dd9ebe708f1e11085f7b368ca12330c5fa7857
vrunda2712 <angy.v27@gmail.com>
2017-06-22 14:54:07 -0400

    Added 3 more tests which successfully runs

--- rocket_test.rb
+++ rocket_test.rb
@@ -12,9 +12,24 @@ class RocketTest < Minitest::Test
     assert @rocket.name == "test"
   end
 
+  def test_name_returns_true_if_string
+    @rocket.name=("test")
+    assert @rocket.name.is_a?(String)
+  end
+
   def test_new_color
     @rocket.colour=("red")
     assert @rocket.colour == "red"
   end
 
+  def test_colour_returns_true_if_string
+    @rocket.colour=("Blue")
+    assert @rocket.colour.is_a?(String)
+  end
+
+  def test_that_non_flying_returns_false_when_lift_off
+    @rocket = Rocket.new
+    # assert @rocket.flying? == false
+     assert @rocket.lift_off == true
+  end
 end
