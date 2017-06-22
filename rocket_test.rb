be3eb45b75280b34a42bb5c07f198285f55bc633
vrunda2712 <angy.v27@gmail.com>
2017-06-22 13:54:54 -0400

    Added 2 test which successfully runs

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,18 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_new_name
+    @rocket.name=("test")
+    assert @rocket.name == "test"
+  end
+
+  def test_new_color
+    @rocket.colour=("red")
+    assert @rocket.colour == "red"
+  end
+
 end
